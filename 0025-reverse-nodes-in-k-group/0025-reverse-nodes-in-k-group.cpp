4766be773c854d6403137d9004d2a4f2c5204245
Joydiipppp <joydip.insight@gmail.com>
2026-06-06 16:23:02 +0530

    Time: 0 ms (100%), Space: 16.3 MB (96.51%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findKthNode(ListNode* temp, int k){
+        k--;
+        while(temp!=NULL && k>0){
+            temp=temp->next;
+            k--;
+        }
+        return temp;
+    }
+    ListNode* reverse(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+
+    while (curr != NULL) {
+        ListNode* nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    return prev;
+}
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        ListNode* prevNode = NULL;
+        while(temp!=NULL){
+            ListNode* kthNode = findKthNode(temp,k);
+            if(kthNode==NULL){
+                if(prevNode){
+                    prevNode->next=temp;
+                }
+                break;
+            }
+            ListNode* nextNode = kthNode->next;
+            kthNode->next = NULL;
+            reverse(temp);
+            if(temp==head){
+                head = kthNode;
+            }
+            else{
+                prevNode->next = kthNode;
+            }
+            prevNode = temp;
+            temp=nextNode;
+
+        }
+        return head;
+    }
+};
